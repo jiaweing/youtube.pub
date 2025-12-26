16036efa9ed3c818ee345bbf36cfc761c520a96f
Jia Wei Ng <jay@base07.com>
2025-12-26 15:46:45 +0800

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,44 @@
+import { expo } from '@better-auth/expo';
+import { polar, checkout, portal } from "@polar-sh/better-auth";
+import { client } from "@youtube.pub/db";
+import { env } from "@youtube.pub/env/server";
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+import { polarClient } from "./lib/payments";
+
+export const auth = betterAuth({
+  database: mongodbAdapter(client),
+  trustedOrigins: [env.CORS_ORIGIN, "mybettertapp://", "exp://"],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      enableCustomerPortal: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "your-product-id",
+              slug: "pro",
+            },
+          ],
+          successUrl: env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+    expo()
+  ],
+});
